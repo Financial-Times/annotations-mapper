864dd9b4d70cc32e287356b8c164d8a14ef0a83d
Clark, Peter <peter.clark@ft.com>
2017-08-24 15:47:58 +0100

    :white_check_mark: Adding test which demonstrates a valid system id getting past the default whitelist.

--- app_test.go
+++ app_test.go
@@ -24,3 +24,20 @@ func TestHandleMessageInvalidWhitelist(t *testing.T) {
 	logLine := hook.LastEntry().Message
 	assert.Equal(t, "Skipping annotations published with Origin-System-Id \"http://cmdb.ft.com/systems/pac\". It does not match the configured whitelist.", logLine)
 }
+
+func TestHandleMessageValidWhitelist(t *testing.T) {
+	whitelist = regexp.MustCompile("http://cmdb.ft.com/systems/methode-web-pub")
+
+	msg := kafka.FTMessage{}
+	msg.Headers = make(map[string]string)
+	msg.Headers["Origin-System-Id"] = "http://cmdb.ft.com/systems/methode-web-pub"
+	msg.Body = ``
+
+	hook := hooks.NewLocal(logger.Log)
+
+	err := handleMessage(msg)
+	assert.Error(t, err) // should fail parsing json
+
+	logLine := hook.LastEntry().Message
+	assert.Equal(t, "Cannot unmarshal message body", logLine)
+}
